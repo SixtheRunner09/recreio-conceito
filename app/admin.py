479fae1e3424b08bcb06a2b17524b0ecd583dab6
Samuel <samuelcordeirodearaujo@gmail.com>
2026-09-26 01:03:46 -0300

    remove idiomas, adiciona produtos pelo django, exibe e filtra por 'destaque' no ProdutoAdmin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Colecao
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco')
+    list_display = ('nome', 'categoria', 'preco', 'preco_antigo', 'destaque')
+    list_filter = ('categoria', 'destaque')
     search_fields = ('nome',)
+
+@admin.register(Colecao)
+class ColecaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ordem')
